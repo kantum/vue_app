97508d9339121ad68814971f1270b4b29e831c34
kantum <kantum@github.com>
2020-01-13 00:19:45 +0100

    sousmenus working

--- src/components/SousMenu1.vue
+++ src/components/SousMenu1.vue
@@ -1,52 +1,46 @@
 <template>
-  <v-container
-    class="ma-0 pa-0"
-    >
+  <v-container class="ma-0 pa-0">
     <v-list>
-      <v-list-item>
-        {{ this.$options.name }}
-      </v-list-item>
+      <v-list-item>{{ this.$options.name }}</v-list-item>
     </v-list>
-    <v-list
-      nav
-      >
+    <v-list nav>
       <v-list-item>
-        <span> current menu: {{ item.menu}}</span>
+        <span>current menu: {{ item.menu}}</span>
       </v-list-item>
-      <v-list-item
-        v-for="item in items"
-        :key="item.title"
-        link
-        >
+      <v-list-item v-for="item in items" :key="item.title" @click="selectItemSub(item)">
         <v-list-item-icon>
           <v-icon>{{ item.icon }}</v-icon>
         </v-list-item-icon>
-      <v-list-item-content>
-        <v-list-item-title>{{ item.title }}</v-list-item-title>
-      </v-list-item-content>
+        <v-list-item-content>
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item-content>
       </v-list-item>
     </v-list>
   </v-container>
-
 </template>
 
 <script>
 export default {
-  name: 'SousMenu1',
+  name: "SousMenu1",
 
-  data () {
+  data() {
     return {
       items: [
-        { title: 'Dashboard', icon: 'mdi-view-dashboard' },
-        { title: 'Photos', icon: 'mdi-image' },
-        { title: 'About', icon: 'mdi-help-box' },
-        { title: 'Test', icon: 'mdi-help-box' },
+        { title: "Dashboard", icon: "mdi-view-dashboard" },
+        { title: "Photos", icon: "mdi-image" },
+        { title: "About", icon: "mdi-help-box" },
+        { title: "Test", icon: "mdi-help-box" }
       ],
-      right: null,
-    }
+      right: null
+    };
   },
   props: {
-    item: Object,
+    item: Object
+  },
+  methods: {
+    selectItemSub(item) {
+      this.$emit("item-selected-sub", item);
+    }
   }
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,14 @@
 <template >
-  <v-container
-    fluid
-    class="ma-0 pa-0 fill-height"
-    >
-    <v-row
-      class="fill-height"
-      no-gutters style="flex-wrap: nowrap;"
-      >
+  <v-container fluid class="ma-0 pa-0 fill-height">
+    <v-row class="fill-height" no-gutters style="flex-wrap: nowrap;">
       <Menu v-on:item-selected="menuItemSelected"></Menu>
-      <v-flex v-if="selectedMenuItem" class="flex-grow-0 flex-shrink-1" >
-        <SousMenu1  :item="selectedMenuItem" ></SousMenu1>
+      <v-flex v-if="selectedMenuItem" class="flex-grow-0 flex-shrink-1">
+        <SousMenu1 v-on:item-selected-sub="menuItemSelectedSub" :item="selectedMenuItem"></SousMenu1>
       </v-flex>
-      <v-flex v-if="selectedMenuItemSub" class="flex-grow-0 flex-shrink-1"  >
-        <SousMenu2></SousMenu2>
+      <v-flex v-if="selectedMenuItemSub" class="flex-grow-0 flex-shrink-1">
+        <SousMenu2 :item="selectedMenuItemSub"></SousMenu2>
       </v-flex>
-      <v-flex  class="flex-grow-1 flex-shrink-0" >
+      <v-flex class="flex-grow-1 flex-shrink-0">
         <Map></Map>
       </v-flex>
     </v-row>
@@ -23,30 +17,33 @@
 
 <script>
 // @ is an alias to /src
-  import Menu from '@/components/Menu.vue'
-import SousMenu1 from '@/components/SousMenu1.vue'
-import SousMenu2 from '@/components/SousMenu2.vue'
-import Map from '@/components/Map.vue'
+import Menu from "@/components/Menu.vue";
+import SousMenu1 from "@/components/SousMenu1.vue";
+import SousMenu2 from "@/components/SousMenu2.vue";
+import Map from "@/components/Map.vue";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
     Menu,
     SousMenu1,
     SousMenu2,
     Map
   },
-  data () {
+  data() {
     return {
       selectedMenuItem: null,
       selectedMenuItemSub: null,
       selectedMenuItemSubDetail: null
-    }
+    };
   },
   methods: {
-    menuItemSelected (item) {
-      this.selectedMenuItem = item
+    menuItemSelected(item) {
+      this.selectedMenuItem = item;
+    },
+    menuItemSelectedSub(item) {
+      this.selectedMenuItemSub = item;
     }
   }
-}
+};
 </script>
