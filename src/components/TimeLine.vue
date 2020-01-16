c5ef676e4091475436385aed351009555b036000
kantum <kantum@github.com>
2020-01-17 00:52:25 +0100

    timeline

--- src/components/TimeLine.vue
+++ src/components/TimeLine.vue
@@ -1,10 +1,10 @@
 <template>
-  <v-bottom-sheet top>
+  <v-bottom-sheet overlay-color="transparent" top>
     <template v-slot:activator="{ on }">
       <v-btn color="primary" dark v-on="on">Timeline</v-btn>
     </template>
     <iframe
-      src="https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=1CgwqeLR4dZP3KrllN5LhgEizl3nJ9-pXXeWM3bVjD6M&font=Default&lang=en&initial_zoom=2&height=350"
+      src="https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=1CgwqeLR4dZP3KrllN5LhgEizl3nJ9-pXXeWM3bVjD6M&font=Default&lang=fr&initial_zoom=1&height=350"
       width="100%"
       height="350"
       webkitallowfullscreen
